ea2626ffc27c82e47fa34e4509ebda8131db70c4
atifeenz <atifeenz@gmail.com>
2026-03-03 20:36:57 +0600

    feat: implement project structure

---
+++ src/test/java/com/__2107027/mini_marketplace/MiniMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.__2107027.mini_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
